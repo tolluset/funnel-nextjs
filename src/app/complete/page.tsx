1f003cc5818f115e3fa3b284a4fc789d211910ed
tolluset <dlqud19@gmail.com>
2024-01-07 20:58:55 +0900

    add project

---
+++ src/app/complete/page.tsx
@@ -0,0 +1,7 @@
+export default function Complete() {
+  return (
+    <main className="flex flex-col items-center justify-between p-24">
+      <p className="text-xl">모든 설정을 완료했어요!</p>
+    </main>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,203 @@
-import Image from 'next/image'
+"use client";
+
+import { useRouter, useSearchParams } from "next/navigation";
+import { useCallback, useEffect, useState } from "react";
+
+const STEPS = ["nickname", "team", "alarm"] as const;
+type Steps = (typeof STEPS)[number];
+
+const FIRST_STEP: Steps = "nickname";
+
+const STEPS_PARAM_PREFIX = "steps";
+const COMPLET_ROUTE = "complete";
+
+type Setup = {
+  nickname: string;
+  team: {
+    a: boolean;
+    b: boolean;
+  };
+  alarm: boolean;
+};
 
 export default function Home() {
+  const [setup, setSetup] = useState<Setup>({
+    nickname: "",
+    team: { a: false, b: false },
+    alarm: false,
+  });
+
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const steps = searchParams.get("steps") as Steps;
+
+  const initRoute = useCallback(() => {
+    router.replace(`?${STEPS_PARAM_PREFIX}=${FIRST_STEP}`);
+  }, [router]);
+
+  const nextStep = (steps: Steps) => {
+    router.push(`?${STEPS_PARAM_PREFIX}=${steps}`);
+  };
+
+  const completeStep = () => {
+    router.push(COMPLET_ROUTE);
+  };
+
+  const registSetup = (
+    regist: Partial<Setup> | ((setup: Setup) => Partial<Setup>),
+  ) => {
+    const next = typeof regist === "function" ? regist(setup) : regist;
+
+    setSetup((prev) => ({
+      ...prev,
+      ...next,
+    }));
+  };
+
+  useEffect(() => {
+    initRoute();
+  }, [initRoute]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+    <main>
+      <div className="flex flex-col items-center justify-between pt-24 ml-[-420px]">
+        {steps === "nickname" && (
+          <NicknameStep
+            setup={setup}
+            registSetup={registSetup}
+            nextStep={() => nextStep("team")}
+          />
+        )}
+        {steps === "team" && (
+          <TeamStep
+            setup={setup}
+            registSetup={registSetup}
+            nextStep={() => nextStep("alarm")}
+          />
+        )}
+        {steps === "alarm" && (
+          <AlarmStep
+            setup={setup}
+            registSetup={registSetup}
+            completeStep={completeStep}
+          />
+        )}
       </div>
     </main>
-  )
+  );
+}
+
+function NicknameStep({
+  setup,
+  registSetup,
+  nextStep,
+}: {
+  setup: Setup;
+  registSetup: (regist: Partial<Setup>) => void;
+  nextStep: () => void;
+}) {
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+
+    registSetup({ nickname: value });
+  };
+
+  return (
+    <div className="flex flex-col gap-y-8">
+      <label>
+        <input
+          type="text"
+          value={setup.nickname}
+          placeholder="닉네임을 입력해주세요."
+          onChange={handleInputChange}
+          className="border"
+        />
+      </label>
+      <NextStepButton onClick={nextStep} />
+    </div>
+  );
+}
+
+function TeamStep({
+  setup,
+  registSetup,
+  nextStep,
+}: {
+  setup: Setup;
+  registSetup: (regist: (setup: Setup) => Partial<Setup>) => void;
+
+  nextStep: () => void;
+}) {
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { id, checked } = e.target;
+
+    const side = id.slice(-1);
+
+    if (side === "a" || side === "b") {
+      registSetup((prev) => ({
+        team: { ...prev["team"], [side]: checked },
+      }));
+    }
+  };
+
+  return (
+    <div className="flex flex-col gap-y-8">
+      <div className="flex flex-col gapy-x-4">
+        <label>
+          <input
+            type="checkbox"
+            id="team-a"
+            checked={setup.team.a}
+            onChange={handleInputChange}
+          />
+          Team A
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            id="team-b"
+            checked={setup.team.b}
+            onChange={handleInputChange}
+          />
+          Team B
+        </label>
+      </div>
+      <NextStepButton onClick={nextStep} />
+    </div>
+  );
+}
+
+function AlarmStep({
+  setup,
+  registSetup,
+  completeStep,
+}: {
+  setup: Setup;
+  registSetup: (regist: Partial<Setup>) => void;
+  completeStep: () => void;
+}) {
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { checked } = e.target;
+
+    registSetup({ alarm: checked });
+  };
+
+  return (
+    <div className="flex flex-col gap-y-8">
+      <label>
+        <input
+          type="checkbox"
+          checked={setup.alarm}
+          onChange={handleInputChange}
+        />
+        알람 받기
+      </label>
+      <NextStepButton onClick={completeStep} />
+    </div>
+  );
+}
+
+function NextStepButton(props: React.ComponentPropsWithoutRef<"button">) {
+  return <button {...props}>다음</button>;
 }
